2e1b18cd3fcabfda4bc1c62afee6d8f2635d055f
fulk2014 <144195753+fulk2014@users.noreply.github.com>
2024-03-23 12:02:57 +0700

    ex04

---
+++ 0275/Lab13_Ex04/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+interface ISpeakable
+{
+    void Speak();
+}
+
+abstract class Animal : IMovable, ISpeakable
+{
+    public abstract void Move();
+    public abstract void Speak();
+}
+
+class Dog : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Dog move by running on the ground");
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine("Dog says: Woof!");
+    }
+}
+
+class Fish : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Fish move by swimming in the water");
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine("Fish cannot speak");
+    }
+}
+
+class Bird : Animal
+{
+    public override void Move()
+    {
+        Console.WriteLine("Bird move by flying in the air");
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine("Bird says: Chirp!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal[] animals = new Animal[3];
+        animals[0] = new Dog();
+        animals[1] = new Bird();
+        animals[2] = new Fish();
+
+        foreach (var animal in animals)
+        {
+            animal.Move();
+            animal.Speak();
+            Console.WriteLine();
+        }
+    }
+}
